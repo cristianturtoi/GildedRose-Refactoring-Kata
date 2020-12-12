a8d8ae665b4b7a6cb51f7f93e1c33ee5e17bcaa2
cristianturtoi <cristi95gabriel@gmail.com>
2020-12-13 01:50:16 +0200

    added unit tests for foo item and only a few for aged brie

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,85 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-class GildedRoseTest {
+public class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void updateQuality_fooItem_decrementSellIn() {
+        Item[] items = new Item[] { new Item("foo", 1, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(0, equalTo(app.items[0].sellIn));
     }
 
+    @Test
+    public void updateQuality_fooItem_decrementQuality() {
+        Item[] items = new Item[] { new Item("foo", 1, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(0, equalTo(app.items[0].quality));
+    }
+
+    @Test
+    public void updateQuality_fooItem_decrementQualityTwice() {
+        Item[] items = new Item[] { new Item("foo", 2, 2) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        assertThat(0, equalTo(app.items[0].sellIn));
+        assertThat(0, equalTo(app.items[0].quality));
+    }
+
+    @Test
+    public void updateQuality_fooItem_negativeQuality() {
+        Item[] items = new Item[] { new Item("foo", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(1, equalTo(app.items[0].sellIn));
+        assertThat(0, equalTo(app.items[0].quality));
+    }
+
+    @Test
+    public void updateQuality_fooItem_negativeSellIn_checkSellIn() {
+        Item[] items = new Item[] { new Item("foo", 0, 2) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(-1, equalTo(app.items[0].sellIn));
+    }
+
+    @Test
+    public void updateQuality_fooItem_negativeSellIn_qualityDecradationTwice() {
+        Item[] items = new Item[] { new Item("foo", 0, 3) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(1, equalTo(app.items[0].quality));
+    }
+
+    @Test
+    public void updateQuality_AgedBrie_decrementSellIn() {
+        Item[] items = new Item[] { new Item("Aged Brie", 1, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(0, equalTo(app.items[0].sellIn));
+    }
+
+    @Test
+    public void updateQuality_AgedBrie_decrementQuality() {
+        Item[] items = new Item[] { new Item("Aged Brie", 1, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(2, equalTo(app.items[0].quality));
+    }
+
+    @Test
+    public void updateQuality_AgedBrie_negativeSellIn_qualityIncreasedTwice() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(-1, equalTo(app.items[0].sellIn));
+        assertThat(3, equalTo(app.items[0].quality));
+    }
 }
